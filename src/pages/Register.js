960df7733dab83abacb0d376ad77887845dbe3ed
iamchiki <nikhil.soni599@gmail.com>
2021-12-16 12:03:17 +0530

    feat: add register new user feature

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import InputComponent from '../components/UI/InputComponent';
+import { auth } from '../firebase/firebase-config';
 
 const Register = () => {
-  const registerHandler = () => {
-    console.log('register');
+  // input values
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+  const confirmPasswordRef = useRef('');
+  console.log(auth);
+  // to navigate to other pages
+  const navigate = useNavigate();
+  // creat new user
+  const registerHandler = async (e) => {
+    e.preventDefault();
+    try {
+      if (passwordRef.current.value === confirmPasswordRef.current.value) {
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          emailRef.current.value,
+          passwordRef.current.value
+        );
+        // navigating to login page after creating new user
+        navigate('/login');
+      } else {
+        throw new Error('Password does not match');
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -13,11 +38,15 @@ const Register = () => {
         onSubmit={registerHandler}
         className='p-8 flex flex-col bg-light-grey rounded-md shadow-lg'>
         <h1 className='text-3xl text-at-light-green mb-4'>Register</h1>
-        <InputComponent label='Email' id='email'></InputComponent>
-        <InputComponent label='Password' id='password'></InputComponent>
-        <InputComponent
-          label='Confirm Password'
-          id='confirm_password'></InputComponent>
+        <InputComponent id='email' ref={emailRef}>
+          Email
+        </InputComponent>
+        <InputComponent id='password' ref={passwordRef}>
+          Password
+        </InputComponent>
+        <InputComponent id='confirm_password' ref={confirmPasswordRef}>
+          Confirm Password
+        </InputComponent>
         <button
           type='submit'
           className='mt-6 py-2 px-6 rounded-sm self-start text-sm
