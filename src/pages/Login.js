b97fa02130b00b0a8c5db4838d947498cd431596
iamchiki <nikhil.soni599@gmail.com>
2021-12-17 16:05:39 +0530

    feat: add log in feature for existing users

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import InputComponent from '../components/UI/InputComponent';
+import { auth } from '../firebase/firebase-config';
 
 const Login = () => {
+  // input values
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+
+  //to navigate to other routes
+  const navigate = useNavigate();
+
+  console.log(auth);
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    try {
+      // login existing user
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        emailRef.current.value,
+        passwordRef.current.value
+      );
+      // navigate to dashboard after sucessfull login
+      navigate('/home');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='max-w-screen-sm mx-auto px-4 py-10'>
-      <form className='p-8 flex flex-col bg-light-grey rounded-md shadow-lg'>
+      <form
+        onSubmit={loginHandler}
+        className='p-8 flex flex-col bg-light-grey rounded-md shadow-lg'>
         <h1 className='text-3xl text-at-light-green mb-4'>Login</h1>
-        <InputComponent label='Email' id='email'></InputComponent>
-        <InputComponent label='Password' id='password'></InputComponent>
+        <InputComponent id='email' ref={emailRef}>
+          Email
+        </InputComponent>
+        <InputComponent id='password' ref={passwordRef}>
+          Password
+        </InputComponent>
         <button
           type='submit'
           className='mt-6 py-2 px-6 rounded-sm self-start text-sm
