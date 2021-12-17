480481b48040134ca74abc5d6583cf65cbc053ec
iamchiki <nikhil.soni599@gmail.com>
2021-12-17 16:03:31 +0530

    feat: add home route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import HeaderComponent from './components/HeaderComponent';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='register' element={<Register></Register>} />
           <Route path='login' element={<Login></Login>}></Route>
+          <Route path='home' element={<Home></Home>}></Route>
         </Routes>
       </main>
     </React.Fragment>
