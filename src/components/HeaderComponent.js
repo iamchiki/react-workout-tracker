a375c446f5c1e9a7153f8970835d4ea69a9000b8
iamchiki <nikhil.soni599@gmail.com>
2021-12-17 16:04:37 +0530

    feat: add logout functionality

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,7 +1,21 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { auth } from '../firebase/firebase-config';
 
 const HeaderComponent = () => {
+  const navigate = useNavigate();
+
+  // logout current user
+  const logoutHandler = async () => {
+    try {
+      const userCredential = await signOut(auth);
+      navigate('/home');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <header className='bg-at-light-green text-white'>
       <nav className='flex justify-around px-4 py-6'>
@@ -10,9 +24,18 @@ const HeaderComponent = () => {
           <h3>Active Tracker</h3>
         </div>
         <ul className='flex justify-between gap-x-6'>
-          <Link to='/'>Home</Link>
-          <Link to='/'>Create</Link>
-          <Link to='/login'>Login</Link>
+          <Link className='cursor-pointer' to='/home'>
+            Home
+          </Link>
+          <Link className='cursor-pointer' to='/'>
+            Create
+          </Link>
+          <Link className='cursor-pointer' to='/login'>
+            Login
+          </Link>
+          <li className='cursor-pointer' onClick={logoutHandler}>
+            Logout
+          </li>
         </ul>
       </nav>
     </header>
