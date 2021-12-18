e1bc0c989071bd5c31b79b57b04940361f96056f
iamchiki <nikhil.soni599@gmail.com>
2021-12-18 23:36:30 +0530

    feat: implement route for create page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HeaderComponent from './components/HeaderComponent';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Create from './pages/Create';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path='register' element={<Register></Register>} />
           <Route path='login' element={<Login></Login>}></Route>
           <Route path='home' element={<Home></Home>}></Route>
+          <Route path='create' element={<Create></Create>}></Route>
         </Routes>
       </main>
     </React.Fragment>
