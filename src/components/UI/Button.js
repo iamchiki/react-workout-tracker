ead0a9200b9b68e866f1816eb557a1d1df59cdb9
iamchiki <nikhil.soni599@gmail.com>
2021-12-19 18:24:38 +0530

    feat: add reusable ui button component

---
+++ src/components/UI/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Button = (props) => {
+  return (
+    <button
+      type='button'
+      className='mt-6 py-2 px-6 rounded-sm self-start text-sm
+text-white bg-at-light-green duration-200 border-solid
+border-2 border-transparent hover:border-at-light-green hover:bg-white
+hover:text-at-light-green'>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
