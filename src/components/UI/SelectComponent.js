dd678db636082b614378a7bfd8f9a84f3b61a613
iamchiki <nikhil.soni599@gmail.com>
2021-12-20 20:43:45 +0530

    feat: conditional display of cardio and strength

--- src/components/UI/SelectComponent.js
+++ src/components/UI/SelectComponent.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 const SelectComponent = (props) => {
+  const changeHandler = (e) => {
+    props.onOptionChange(e.target.value);
+  };
+
   return (
     <div className='flex flex-col'>
-      <label
-        htmlFor='workout-type'
-        className='mb-1 text-sm text-at-light-green'>
+      <label htmlFor={props.id} className='mb-1 text-sm text-at-light-green'>
         {props.children}
       </label>
-      <select id={props.id} className='p-2 text-gray-500 focus:outline-none'>
+      <select
+        id={props.id}
+        onChange={changeHandler}
+        className='p-2 text-gray-500 focus:outline-none'>
         {props.options.map((option, index) => {
           return (
             <option value={option.value} key={index}>
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import CardioInput from '../components/CardioInput';
+import StrengthInput from '../components/StrengthInput';
+import Button from '../components/UI/Button';
 import InputComponent from '../components/UI/InputComponent';
 import SelectComponent from '../components/UI/SelectComponent';
 
+const initialState = { strength: false, cardio: false };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'strength':
+      return {
+        strength: true,
+        cardio: false,
+      };
+    case 'cardio':
+      return {
+        strength: false,
+        cardio: true,
+      };
+    default:
+      return initialState;
+  }
+};
+
 const Create = () => {
+  const [workoutState, dispatch] = useReducer(reducer, initialState);
   const workoutTypeOptions = [
     { label: 'Select Workout', value: 'select-workout' },
     { label: 'Strength Training', value: 'strength' },
     { label: 'Cardio', value: 'cardio' },
   ];
+
+  const optionChangeHandler = (optionValue) => {
+    dispatch({ type: optionValue });
+  };
+
   return (
     <div className='max-w-screen-sm mx-auto px-4 py-10'>
       <form className='p-8 flex flex-col bg-light-grey rounded-md shadow-lg'>
         <h1 className='text-3xl text-at-light-green mb-4'>Record Workout</h1>
         <InputComponent id='workout-name'>Workout Name</InputComponent>
-        <SelectComponent id='workout-type' options={workoutTypeOptions}>
+        <SelectComponent
+          onOptionChange={optionChangeHandler}
+          id='workout-type'
+          options={workoutTypeOptions}>
           Workout Type
         </SelectComponent>
-        <button
-          type='submit'
-          className='mt-6 py-2 px-6 rounded-sm self-start text-sm
-      text-white bg-at-light-green duration-200 border-solid
-      border-2 border-transparent hover:border-at-light-green hover:bg-white
-      hover:text-at-light-green'>
-          Record Workout
-        </button>
+        {workoutState.strength && <StrengthInput></StrengthInput>}
+        {workoutState.cardio && <CardioInput></CardioInput>}
+        <Button>Record Workout</Button>
       </form>
     </div>
   );
