e16564ef0f20981002543f2f33af07d17ea183aa
iamchiki <nikhil.soni599@gmail.com>
2022-01-03 17:17:02 +0530

    feat: add functionality to delete exercise input fields

--- src/pages/StrengthEdit.js
+++ src/pages/StrengthEdit.js
@@ -17,9 +17,31 @@ const StrengthEdit = (props) => {
     ctx.exercises[props.rowIndex][id] = value;
     setInputValues({ ...inputValues, [id]: value });
   };
+
+  const deleteHandler = () => {
+    props.deleteExercise(props.rowIndex);
+  };
+
   console.log(ctx.exercises);
   return (
     <div className='flex flex-col gap-x-6 gap-y-2 relative sm:flex-row'>
+      {props.editMode && (
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 48 48'
+          className='h-4 w-auto absolute -left-5 cursor-pointer'
+          onClick={deleteHandler}>
+          <path
+            fill='#44ba9f'
+            d='M30.6,44H17.4c-2,0-3.7-1.4-4-3.4L9,11h30l-4.5,29.6C34.2,42.6,32.5,44,30.6,44z'
+          />
+          <path fill='#286d5d' d='M28 6L20 6 14 12 34 12z' />
+          <path
+            fill='#286d5d'
+            d='M10,8h28c1.1,0,2,0.9,2,2v2H8v-2C8,8.9,8.9,8,10,8z'
+          />
+        </svg>
+      )}
       <div className='flex flex-2 flex-col md:w-1/3'>
         <label className='mb-1 text-sm text-at-light-green'>Exercise</label>
         {props.editMode && (
