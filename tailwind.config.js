eab55994ec45a2d957ebaa2034e11794cb88b965
iamchiki <nikhil.soni599@gmail.com>
2022-01-14 19:39:36 +0530

    chore: change tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
       'at-light-green': '#44ba9f',
       'at-green': '#286d5d',
       'light-grey': '#f1f1f1',
+      white: '#fff',
+      'red-500': '#ef4444',
+      transparent: 'transparent',
+    },
+    screens: {
+      sm: '640px',
+
+      md: '768px',
+
+      lg: '1024px',
+
+      xl: '1024px',
+
+      '2xl': '1024px',
     },
   },
   plugins: [],
